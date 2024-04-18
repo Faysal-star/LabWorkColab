2d59c0691ebef1b4bb76d719f5efa1aefb19e3d2
Faysal-star <kishorepasha347@gmail.com>
2024-04-18 09:36:35 +0600

    faysal

--- app/src/androidTest/java/com/example/lab8/MainActivityTest.java
+++ app/src/androidTest/java/com/example/lab8/MainActivityTest.java
@@ -1,7 +1,49 @@
 package com.example.lab8;
 
+import androidx.test.espresso.Espresso;
+import androidx.test.espresso.action.ViewActions;
+import androidx.test.espresso.assertion.ViewAssertions;
+import androidx.test.espresso.matcher.ViewMatchers;
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+
 import org.junit.Rule;
+import org.junit.Test;
 
 public class MainActivityTest {
+    @Rule
+    public ActivityScenarioRule<MainActivity> activityScenarioRule =
+            new ActivityScenarioRule<>(MainActivity.class);
+
+    @Test
+    public void beginning(){
+        Espresso.onView(ViewMatchers.withId(R.id.textView))
+                .check(ViewAssertions.matches(ViewMatchers.withText("Button clicked 0 times")));
+    }
+
+    @Test
+    public void testButtonClick_1() {
+        Espresso.onView(ViewMatchers.withId(R.id.button)).perform(ViewActions.click());
+
+        Espresso.onView(ViewMatchers.withId(R.id.textView))
+                .check(ViewAssertions.matches(ViewMatchers.withText("Button clicked 1 times")));
+    }
+
+    @Test
+    public void testButtonClick_2() {
+        Espresso.onView(ViewMatchers.withId(R.id.button))
+                .perform(ViewActions.click())
+                .perform(ViewActions.click());
+
+        Espresso.onView(ViewMatchers.withId(R.id.textView))
+                .check(ViewAssertions.matches(ViewMatchers.withText("Button clicked 2 times")));
+    }
+
+    @Test
+    public void testButton2Click() {
+        Espresso.onView(ViewMatchers.withId(R.id.button2)).perform(ViewActions.click());
+
+        Espresso.onView(ViewMatchers.withId(R.id.textView))
+                .check(ViewAssertions.matches(ViewMatchers.withText("Button clicked -1 times")));
+    }
 
 }
--- app/src/main/java/com/example/lab8/MainActivity.java
+++ app/src/main/java/com/example/lab8/MainActivity.java
@@ -3,12 +3,33 @@ package com.example.lab8;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    public TextView textView;
+    public Button button , button2;
+    public int i = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textView = findViewById(R.id.textView);
+        button = findViewById(R.id.button);
+        button2 = findViewById(R.id.button2);
+
+        textView.setText("Button clicked 0 times");
+
+        button.setOnClickListener(v -> {
+            i++;
+            textView.setText("Button clicked " + i + " times");
+        });
+
+        button2.setOnClickListener(v -> {
+            i--;
+            textView.setText("Button clicked " + i + " times");
+        });
     }
 }
